08f58d3c42c5f9c97e548815221c6ef0ae4bfb17
Jetawo Oloruntoba <jetawotobajetex@gmail.com>
2026-05-26 15:48:47 +0100

    content update

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "Public")));
 //checkEnvironment();
 const PORT = process.env.PORT;
 
